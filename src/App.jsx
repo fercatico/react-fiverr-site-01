3eb923a1212641e68967ce9c9245f584de26f9f9
Miguel Fernandez <miguelfera@gmail.com>
2023-02-15 12:11:54 -0600

    Add Navbar Component and its logic

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nemo velit non iusto ab dicta necessitatibus eveniet illo consequatur! Autem ratione a quas quis eum magni iusto dolorum sint temporibus?</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
