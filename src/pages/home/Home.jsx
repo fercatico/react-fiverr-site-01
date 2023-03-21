669a90c02adb53148439415749df8189e82b0470
Miguel Fernandez <miguelfera@gmail.com>
2023-03-20 21:40:56 -0600

    Basic Layout Components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import "./Home.scss";
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
+import { cards, projects } from "../../data";
+import Features from "../../components/features/Features";
+import Explore from "../../components/explore/Explore";
+import FeaturesDark from "../../components/features/FeaturesDark";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
+      <Features />
+      <Explore />
+      <FeaturesDark />
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
